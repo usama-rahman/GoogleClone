ba631f5b64b3990bdb0f383aa181bd465cff922c
Usama Rahman <rahmanusama390@gmail.com>
2023-11-08 23:23:02 +0600

    Next

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Header from "./Components/FrontPage/Header";
+
 function App() {
   return (
-    <>
+    <main>
+      {/* #bdc1c6 */}
+      <Header />
       <h1>hello world</h1>
-    </>
+    </main>
   );
 }
 
