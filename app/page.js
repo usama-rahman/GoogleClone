054dac10b9bfc0d673340a03203b9979c46fda59
Usama Rahman <rahmanusama390@gmail.com>
2023-11-23 00:27:57 +0600

    Cool

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import GoogleImg from "./Components/GoogleImg";
 import SearchIcon from "./Components/SearchIcon";
 import LeftNavItems from "./Components/LeftNavItems";
 import RightNavItems from "./Components/RightNavItems";
+import MicrophoneIcon from "./Components/MicrophoneIcon";
 
 export default function Home() {
   return (
@@ -10,26 +11,14 @@ export default function Home() {
         <LeftNavItems />
         <RightNavItems />
       </header>
-      <form>
+
+      <form className="flex flex-col mt-44 items-center flex-grow">
         <GoogleImg />
 
-        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border-gray-200 ">
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full items-center  px-5 py-3 sm:max-w-xl inputSection  lg:max-w-2xl ">
           <SearchIcon />
-          <input type="text" className="focus:outline-none flex-grow  " />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z"
-            />
-          </svg>
+          <input type="text" className="focus:outline-none flex-grow " />
+          <MicrophoneIcon />
         </div>
       </form>
     </div>
