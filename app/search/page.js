4c26ab426586188a061b3e018db99d1ac1087e2e
Usama Rahman <rahmanusama390@gmail.com>
2023-11-24 18:50:46 +0600

    !

--- app/search/page.js
+++ app/search/page.js
@@ -1,24 +1,11 @@
 "use client";
 
-// function Search() {
-//   return (
-//     <div>
-//       <Head>
-//         <title>Search Results</title>
-//         <link rel="icon" href="/favicon.ico"></link>
-//       </Head>
-
-//       <Header />
-//     </div>
-//   );
-// }
-
-// export default Search;
+import SearchHeader from "../Components/SearchHeader";
 
 function Search() {
   return (
     <div>
-      <h1>Usama</h1>
+      <SearchHeader />
     </div>
   );
 }
